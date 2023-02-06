814118a04131183e70de134f8ec3dffc7783ceac
Amirhossein-mousavi <119420193+Amirhossein-mousavi@users.noreply.github.com>
2023-02-06 16:29:49 +0330

    application completed

--- app/src/main/java/ir/reversedev/mycalendar/model/dao/FavoriteDao.kt
+++ app/src/main/java/ir/reversedev/mycalendar/model/dao/FavoriteDao.kt
@@ -2,13 +2,14 @@ package ir.reversedev.mycalendar.model.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import ir.reversedev.mycalendar.model.data.FavoriteEntity
 
 @Dao
 interface FavoriteDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFavorite(favoriteEntity: FavoriteEntity)
 
     @Query("DELETE FROM tb_favorite WHERE name = :name")
